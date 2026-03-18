63bd08ed47416a9150f0493e56b23afbcba4a773
meghraj161920 <meghrajhamade9@gmail.com>
2026-03-18 11:00:49 +0530

    Made some changes and activated all the apps also create url patterns and views , for test purposr , target is to complete all the templates today.

--- core/views.py
+++ core/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def terms(request):
+    return render(request, 'core/terms.html')
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def reviews(request):
+    return render(request, 'core/home.html')  # temporary
